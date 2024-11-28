57e9bfef9c9b6e50ba4718ca4ce27d0b8dfa3b88
johanliebertjohan <simontebroke@proton.me>
2024-11-28 20:31:28 +0100

    3223

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import StartSection from "./components/StartSection";
 import SectionOne from "./components/SectionOne";
 import GridSection from "./components/GridSection";
 import TabsSlider from "./components/TabsSlider";
+import EmblaCarousel from "./components/EmblaCarousel";
+import "./components/embla.css";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <div className="App">
         <Navbar />
         <StartSection />
+        <EmblaCarousel slides={[1, 2, 3, 4, 5]} />
         <TabsSlider />
         <SectionOne />
         <GridSection />
---
+++ src/components/EmblaCarousel.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons,
+} from "./EmblaCarouselArrowButtons";
+import {
+  SelectedSnapDisplay,
+  useSelectedSnapDisplay,
+} from "./EmblaCarouselSelectedSnapDisplay";
+import useEmblaCarousel from "embla-carousel-react";
+
+function EmblaCarousel(props) {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  const { selectedSnap, snapCount } = useSelectedSnapDisplay(emblaApi);
+
+  return (
+    <section className="embla">
+      <div className="embla__controls">
+        <p className="heading">ChatGPT</p>
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+      </div>
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <div
+                className="embla__slide__image"
+                style={{ backgroundImage: `url('/${index}.jpg')` }}
+                alt=""
+              >
+                <p className="company">
+                  {index == 1
+                    ? "Apple"
+                    : index == 2
+                    ? "Starbucks"
+                    : index == 3
+                    ? "IKEA"
+                    : "Product"}
+                </p>
+                <p className="logo">fe</p>
+                <p className="description">
+                  Very modern looking site with clean grey and white tones.
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default EmblaCarousel;
